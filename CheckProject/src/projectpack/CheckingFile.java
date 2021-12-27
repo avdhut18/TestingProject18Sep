99b37fc741a6221bf9685dd80af1d465da901b61
avdhu <avdhu@LAPTOP-J5E37TJF>
2021-12-27 09:50:32 +0530

    1st commit 27 dec 2021

---
+++ CheckProject/src/projectpack/CheckingFile.java
@@ -0,0 +1,10 @@
+package projectpack;
+
+public class CheckingFile {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("GitHub Project");
+	}
+
+}
